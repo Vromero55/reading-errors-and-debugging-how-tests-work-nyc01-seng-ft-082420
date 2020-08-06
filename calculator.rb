1aa1ff6e268900159e9c9c1e2841562adc1acdf9
Victor Romero <romeroa.victor@yahoo.com>
2020-08-06 21:36:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@ first_number=5.25;# Add your variables here
 second_number=5;
 sum=first_number + second_number;
 difference=first_number - second_number;
-product= first_number * second_number;
+product=first_number * second_number;
+quotient=
